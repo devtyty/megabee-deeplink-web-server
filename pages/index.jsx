d0aaa3393801fce174c61277249de759b4d05afc
Ty. Trương Văn <tytv@vng.com.vn>
2022-10-02 15:27:11 +0700

    Update docker compose

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to MegaBee's Home!
+          Welcome to MegaBee's Home sdsdds!
         </h1>
 
         <p className={styles.description}>
